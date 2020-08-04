6c9feaaeb289d7145f6225e0544fdb42e2090eac
Maxim van Hoorn <mvanhoorn1@babson.edu>
2020-08-04 11:32:47 +0200

    created some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+	Blog.create!(
+		string:"My Blog Post #{blog}",
+		body:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 
+		, comes from a line in section 1.10.32."
+	) 
+end 
+
+p "10 blog Post created"
+
+5.times do |skills|
+	Skill.create!(
+		title:"Rials #{skills}",
+		percent_utilized: 15
+
+	)
+end
+
+
+
+p "10 Skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+	title:"Portfolio item #{portfolio_item}",
+    subtitle: "My great service",
+    body:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 
+		, comes from a line in section 1.10.32.",
+    main_image:"http://placehold.it/600x400",
+    thumb_image:"http://placehold.it/350x200"
+
+	)
+end
+
+p "9 portfolio items created"
